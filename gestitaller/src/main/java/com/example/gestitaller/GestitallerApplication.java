34a6a829de434d04590a5540f758cb4ddfac5e92
Santi Stars <santi_r6@hotmail.es>
2021-12-29 12:46:46 +0100

    Proyecto inicial Gestitaller

---
+++ gestitaller/src/main/java/com/example/gestitaller/GestitallerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestitaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestitallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestitallerApplication.class, args);
+	}
+
+}
---
+++ gestitaller/src/test/java/com/example/gestitaller/GestitallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestitaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestitallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
